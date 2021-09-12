86e7603a41303deb012cdb48ac47b2f960570185
Hunter Cox <huntersamuelcox1992@gmail.com>
2021-09-12 11:31:19 -0400

    Create a component for recent work photos. Might eventually turn this into a widget.

---
+++ inc/components/recent-work.php
@@ -0,0 +1,15 @@
+<?php
+// make a custom field to add a carousel of recent work as pictures
+// add this to the Site Options page in the WP Admin
+
+$images = get_field('gallery');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image_id ): ?>
+            <li>
+                <?php echo wp_get_attachment_image( $image_id, $size ); ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
