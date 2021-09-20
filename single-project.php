a5dceefbeb4d39e1d6ee32dd153ac297f6e077b0
Hunter Cox <huntersamuelcox1992@gmail.com>
2021-09-20 11:57:23 -0400

    Output all ACF values to single-project template. (Ready to style)

--- single-project.php
+++ single-project.php
@@ -20,6 +20,66 @@
             <?php the_content(); ?>
           </div><!-- /.page__content -->
 
+          <div class="project-breakdown">
+            <?php if ( have_rows('project_sections') ) : ?>
+
+              <?php while( have_rows('project_sections') ) : the_row(); ?>
+
+                <?php
+                  $section_name = get_sub_field('section_name');
+                  $description  = get_sub_field('section_description');
+                  $tasks        = get_sub_field('section_tasks');
+                  $services     = get_sub_field('section_services');
+                  $areas        = get_sub_field('section_areas');
+
+                  if ( $section_name ) {
+                    echo '<h2>'.$section_name.'</h2>';
+                  }
+
+                  if ( $description ) {
+                    echo '<p>'.$description.'</p>';
+                  }
+
+                  if ($services) {
+                    echo '<h4>'.$section_name.' - Services</h4>';
+                    echo '<ul class="section-services">';
+                    foreach($services as $service) {
+                      echo '<li><strong>'. esc_html( $service->name ) .'</strong></li>';
+                    }
+                    echo '</ul>';
+                  }
+
+                  if ($areas) {
+                    echo '<h4>'.$section_name.' - Areas</h4>';
+                    echo '<ul class="section-areas">';
+                    foreach($areas as $area) {
+                      echo '<li><strong>'. esc_html( $area->name ) .'</strong></li>';
+                    }
+                    echo '</ul>';
+                  }
+
+                  if ( have_rows('section_tasks') ) :
+
+                    while( have_rows('section_tasks') ) : the_row();
+
+
+                      $task_name = get_sub_field('task_name');
+
+                      echo 'Task: '.$task_name.'<br />';
+
+                    endwhile;
+
+                  endif;
+
+                ?>
+
+
+              <?php endwhile; ?>
+
+            <?php endif; ?>
+
+          </div><!-- /.project-breakdown -->
+
         </article>
       <?php endwhile; ?>
       <?php else : ?>
